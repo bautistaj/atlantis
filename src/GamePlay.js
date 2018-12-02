61f10facc667faf780775313fbef3f176392bd79
Jose Luis Campos Butista <bautistaj20@hotmail.com>
2018-12-02 14:13:36 -0600

    Sprite is added and events are applied.

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -4,17 +4,46 @@ GamePlayManager = {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
+
+    this.flagFirstMouseDown =  false;
   },
 
   preload: function(){
     game.load.image('background',"assets/images/background.png");
+    game.load.spritesheet("horse","assets/images/horse.png",84, 156, 2);
   },
 
   create: function(){
       game.add.sprite(0,0,"background");
-  },
+      this.horse = game.add.sprite(0,0,"horse");
+      this.horse.frame = 1;
 
+      this.horse.x = game.width/2;
+      this.horse.y = game.height/2;
+      this.horse.anchor.setTo(0.5,0.5);
+      this.horse.angle = 0;
+      //this.horse.scale.setTo(1,2);
+      //this.horse.alpha = 0.5;
+
+      game.input.onDown.add(this.onTap, this);
+  },
+  onTap: function(){
+    this.flagFirstMouseDown =  true;
+  },
   update: function(){
+    if(this.flagFirstMouseDown){
+      var pointerX = game.input.x;
+      var pointerY = game.input.y;
+
+      var distX = pointerX - this.horse.x;
+      var distY = pointerY - this.horse.y;
+
+
+      this.horse.scale.setTo( distX > 0 ? 1 : -1 ,1);
+      this.horse.x += distX * 0.05;
+      this.horse.y += distY * 0.05;
+    }
+
   }
 }
 
